4daeac9affd62217f4bafb8d7b8ec20834ba2a14
marianux <llamedom@gmail.com>
2024-10-18 11:12:14 -0300

    IO de señales

---
+++ lectura_sigs.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  8 19:55:30 2023
+
+@author: mariano
+"""
+
+import numpy as np
+from scipy import signal as sig
+
+import matplotlib.pyplot as plt
+   
+import scipy.io as sio
+from scipy.io.wavfile import write
+
+def vertical_flaten(a):
+
+    return a.reshape(a.shape[0],1)
+
+##################
+# Lectura de ECG #
+##################
+
+fs_ecg = 1000 # Hz
+
+# para listar las variables que hay en el archivo
+#io.whosmat('ECG_TP4.mat')
+mat_struct = sio.loadmat('./ECG_TP4.mat')
+
+ecg_one_lead = vertical_flaten(mat_struct['ecg_lead'])
+N = len(ecg_one_lead)
+
+hb_1 = vertical_flaten(mat_struct['heartbeat_pattern1'])
+hb_2 = vertical_flaten(mat_struct['heartbeat_pattern2'])
+
+plt.figure()
+plt.plot(ecg_one_lead[5000:12000])
+
+plt.figure()
+plt.plot(hb_1)
+
+plt.figure()
+plt.plot(hb_2)
+
+####################################
+# Lectura de pletismografía (PPG)  #
+####################################
+
+fs_ppg = 400 # Hz
+
+# Cargar el archivo CSV como un array de NumPy
+ppg = np.genfromtxt('PPG.csv', delimiter=',', skip_header=1)  # Omitir la cabecera si existe
+
+plt.figure()
+plt.plot(ppg)
+
+####################
+# Lectura de audio #
+####################
+
+# Cargar el archivo CSV como un array de NumPy
+fs_audio, wav_data = sio.wavfile.read('la cucaracha.wav')
+# fs_audio, wav_data = sio.wavfile.read('prueba psd.wav')
+# fs_audio, wav_data = sio.wavfile.read('silbido.wav')
+
+plt.figure()
+plt.plot(wav_data)
+
+# si quieren oirlo, tienen que tener el siguiente módulo instalado
+# pip install sounddevice
+# import sounddevice as sd
+# sd.play(wav_data, fs_audio)
+
